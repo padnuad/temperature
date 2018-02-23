b945d986da9450e900a87a0893146c36dfebbbbd
padnuad <padnuad@gmail.com>
2018-02-23 13:52:14 +0700

    Create netpiet.ino

---
+++ netpiet.ino
@@ -0,0 +1,145 @@
+/*  NETPIE ESP8266 basic sample                            */
+/*  More information visit : https://netpie.io             */
+
+#include <ESP8266WiFi.h>
+#include <MicroGear.h>
+#include "DHTesp.h"
+
+const char* ssid     = "My Wireless Network 2_1";
+const char* password = "";
+
+#define APPID   "TemperHum"
+#define KEY     "ifU2ScYSNQ041Js"
+#define SECRET  "wJ5zGnnH97l6wFP3jgOiZYmOb"
+#define ALIAS   "ppop"
+
+WiFiClient client;
+DHTesp dht;
+char str [32];
+int timer = 0;
+MicroGear microgear(client);
+
+/* If a new message arrives, do this */
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Incoming message --> ");
+    msg[msglen] = '\0';
+    Serial.println((char *)msg);
+    String txt = ((char *)msg);
+    Serial.println(txt);
+    if(txt="ON"){
+              digitalWrite(16, HIGH);
+              microgear.publish("/sw","ON");
+          }
+}
+
+void onFoundgear(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Found new member --> ");
+    for (int i=0; i<msglen; i++)
+        Serial.print((char)msg[i]);
+    Serial.println();  
+}
+
+void onLostgear(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.print("Lost member --> ");
+    for (int i=0; i<msglen; i++)
+        Serial.print((char)msg[i]);
+    Serial.println();
+}
+
+/* When a microgear is connected, do this */
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
+    Serial.println("Connected to NETPIE...");
+    /* Set the alias of this microgear ALIAS */
+    microgear.setAlias(ALIAS);
+}
+
+
+void setup() {
+  dht.setup(4);
+    /* Add Event listeners */
+pinMode(16, OUTPUT);
+    /* Call onMsghandler() when new message arraives */
+    microgear.on(MESSAGE,onMsghandler);
+
+    /* Call onFoundgear() when new gear appear */
+    microgear.on(PRESENT,onFoundgear);
+
+    /* Call onLostgear() when some gear goes offline */
+    microgear.on(ABSENT,onLostgear);
+
+    /* Call onConnected() when NETPIE connection is established */
+    microgear.on(CONNECTED,onConnected);
+
+    Serial.begin(115200);
+    Serial.println("Starting...");
+
+    /* Initial WIFI, this is just a basic method to configure WIFI on ESP8266.                       */
+    /* You may want to use other method that is more complicated, but provide better user experience */
+    if (WiFi.begin(ssid, password)) {
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.print(".");
+        }
+    }
+
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    /* Initial with KEY, SECRET and also set the ALIAS here */
+    microgear.init(KEY,SECRET,ALIAS);
+
+    /* connect to NETPIE to a specific APPID */
+    microgear.connect(APPID);
+}
+
+void loop() {
+    /* To check if the microgear is still connected */
+    if (microgear.connected()) {
+        Serial.println("connected");
+
+        /* Call this method regularly otherwise the connection may be lost */
+        microgear.loop();
+
+        if (timer >= 1000) {
+            Serial.println("Publish...");
+        
+  delay(dht.getMinimumSamplingPeriod());
+
+  float humidity = dht.getHumidity();
+  float temperature = dht.getTemperature();
+
+ 
+  Serial.println(humidity, 1);
+
+  Serial.println(temperature, 1);
+  sprintf(str,"temperature:%.2f,humidity:%.2f",temperature,humidity);
+microgear.writeFeed("TemperatureAndHumidity",str);
+microgear.publish("/dht1",temperature, 1);
+microgear.publish("/dht2",humidity, 1);
+Serial.println(str);
+  if(temperature > 27)
+{
+ digitalWrite(16, HIGH);
+ microgear.publish("/sw","ON");
+}
+ else if(temperature < 26.5)
+{
+ digitalWrite(16, LOW);
+ microgear.publish("/sw","OFF");
+}
+
+            timer = 0;
+        } 
+        else timer += 100;
+    }
+    else {
+        Serial.println("connection lost, reconnect...");
+        if (timer >= 5000) {
+            microgear.connect(APPID);
+            timer = 0;
+        }
+        else timer += 100;
+    }
+    delay(100);
+}
